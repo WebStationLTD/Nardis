9229ef6c046b01a7f145749e9630a4a71b4b02ba
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-03-18 17:15:06 +0200

    Fix links in all products page

--- app/products/productsList.js
+++ app/products/productsList.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function ProductsList({ products }) {
   return (
     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-6">
       {products.length > 0 ? (
         products.map((product) => (
-          <a key={product.id} href={`/product/${product.id}`} className="group">
+          <Link key={product.id} href={`/products/${product.slug}`} className="group">
             <img
               alt={product.name}
               src={product.images?.[0]?.src || "/placeholder.jpg"}
@@ -13,7 +15,7 @@ export default function ProductsList({ products }) {
             <p className="mt-1 text-lg font-medium text-gray-900">
               {product.price} {product.currency || "лв."}
             </p>
-          </a>
+          </Link>
         ))
       ) : (
         <p className="text-center text-gray-500">Няма налични продукти.</p>
