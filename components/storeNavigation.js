6d218b4af6ee690cda352c93c56b16b19aadb4db
7onc1 <anton.demirev84@gmail.com>
2025-03-23 11:58:35 +0200

    Update
    Improve mega menu - add images

--- components/storeNavigation.js
+++ components/storeNavigation.js
@@ -50,127 +50,32 @@ export default function Example({ categories }) {
         name: "Магазин",
         featured: [
           {
-            name: "New Arrivals",
-            href: "#",
-            imageSrc:
-              "https://tailwindcss.com/plus-assets/img/ecommerce-images/mega-menu-category-01.jpg",
-            imageAlt:
-              "Models sitting back to back, wearing Basic Tee in black and bone.",
+            name: "Всички продукти",
+            href: "/products",
+            imageSrc: "./nardis-online-shop-for-luxury-cosmetics.jpg",
+            imageAlt: "Кателог продукти на Nardis.bg",
           },
           {
-            name: "Basic Tees",
+            name: "Artdeco ASIAN SPA",
             href: "#",
-            imageSrc:
-              "https://tailwindcss.com/plus-assets/img/ecommerce-images/mega-menu-category-02.jpg",
+            imageSrc: "./artdeco-asian-spa-mega-menu-bg.jpg",
             imageAlt:
               "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
           },
           {
-            name: "Accessories",
+            name: "Грим",
             href: "#",
-            imageSrc:
-              "https://tailwindcss.com/plus-assets/img/ecommerce-images/mega-menu-category-03.jpg",
+            imageSrc: "./makeup-mega-menu-bg.jpg",
             imageAlt:
               "Model wearing minimalist watch with black wristband and white watch face.",
           },
         ],
       },
-      {
-        id: "men",
-        name: "Грижа за тяло",
-        featured: [
-          {
-            name: "Accessories",
-            href: "#",
-            imageSrc:
-              "https://tailwindcss.com/plus-assets/img/ecommerce-images/home-page-03-category-01.jpg",
-            imageAlt:
-              "Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters.",
-          },
-          {
-            name: "New Arrivals",
-            href: "#",
-            imageSrc:
-              "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg",
-            imageAlt:
-              "Drawstring top with elastic loop closure and textured interior padding.",
-          },
-          {
-            name: "Artwork Tees",
-            href: "#",
-            imageSrc:
-              "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-02-image-card-06.jpg",
-            imageAlt:
-              "Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.",
-          },
-        ],
-        sections: [
-          [
-            {
-              id: "shoes",
-              name: "Shoes & Accessories",
-              items: [
-                { name: "Sneakers", href: "#" },
-                { name: "Boots", href: "#" },
-                { name: "Sandals", href: "#" },
-                { name: "Socks", href: "#" },
-              ],
-            },
-            {
-              id: "collection",
-              name: "Shop Collection",
-              items: [
-                { name: "Everything", href: "#" },
-                { name: "Core", href: "#" },
-                { name: "New Arrivals", href: "#" },
-                { name: "Sale", href: "#" },
-              ],
-            },
-          ],
-          [
-            {
-              id: "clothing",
-              name: "All Clothing",
-              items: [
-                { name: "Basic Tees", href: "#" },
-                { name: "Artwork Tees", href: "#" },
-                { name: "Pants", href: "#" },
-                { name: "Hoodies", href: "#" },
-                { name: "Swimsuits", href: "#" },
-              ],
-            },
-            {
-              id: "accessories",
-              name: "All Accessories",
-              items: [
-                { name: "Watches", href: "#" },
-                { name: "Wallets", href: "#" },
-                { name: "Bags", href: "#" },
-                { name: "Sunglasses", href: "#" },
-                { name: "Hats", href: "#" },
-                { name: "Belts", href: "#" },
-              ],
-            },
-          ],
-          [
-            {
-              id: "brands",
-              name: "Brands",
-              items: [
-                { name: "Re-Arranged", href: "#" },
-                { name: "Counterfeit", href: "#" },
-                { name: "Full Nelson", href: "#" },
-                { name: "My Way", href: "#" },
-              ],
-            },
-          ],
-        ],
-      },
     ],
     pages: [
-      { name: "Company", href: "#" },
-      { name: "Stores", href: "#" },
-      { name: "Shop", href: "/products" },
+      { name: "За нас", href: "/about-us" },
+      { name: "Блог", href: "/blog" },
+      { name: "Контакти", href: "/contacts" },
     ],
   };
 
@@ -246,7 +151,7 @@ export default function Example({ categories }) {
                                 aria-hidden="true"
                                 className="mt-0.5 text-gray-700 sm:mt-1"
                               >
-                                Shop now
+                                Пазарувайте сега
                               </p>
                             </div>
                           </div>
@@ -440,7 +345,7 @@ export default function Example({ categories }) {
                                           aria-hidden="true"
                                           className="mt-0.5 text-gray-700 sm:mt-1"
                                         >
-                                          Shop now
+                                          Пазарувайте сега
                                         </p>
                                       </div>
                                     </div>
