89dc47e5e7fcc2fc35caab644d19470591dde1cb
7onc1 <anton.demirev84@gmail.com>
2025-03-24 14:38:05 +0200

    Add configuration

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@ const nextConfig = {
         hostname: "nardis.rosset.website",
       },
     ],
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 };
 
