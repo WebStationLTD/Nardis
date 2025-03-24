8a4d4bd81af303a91632ff047319610bca48b2bc
7onc1 <anton.demirev84@gmail.com>
2025-03-24 18:33:39 +0200

    Fix - navigation close when anchor is clicked

--- components/storeNavigation.js
+++ components/storeNavigation.js
@@ -288,137 +288,143 @@ export default function Example({ categories }) {
                 <div className="flex h-full space-x-8">
                   {navigation.categories.map((category) => (
                     <Popover key={category.name} className="flex">
-                      <div className="relative flex">
-                        <PopoverButton className="group relative z-10 flex items-center cursor-pointer justify-center text-xl font-medium text-white transition-colors duration-200 ease-out hover:text-white data-open:text-white">
-                          {category.name}
-                          <span
-                            aria-hidden="true"
-                            className="absolute inset-x-0 bottom-0 h-1 transition-colors duration-200 ease-out group-data-open:bg-[#b3438f] sm:mt-5 sm:translate-y-px sm:transform"
-                          />
-                        </PopoverButton>
-                      </div>
+                      {({ open, close }) => (
+                        <>
+                          <div className="relative flex">
+                            <PopoverButton className="group relative z-10 flex items-center cursor-pointer justify-center text-xl font-medium text-white transition-colors duration-200 ease-out hover:text-white data-open:text-white">
+                              {category.name}
+                              <span
+                                aria-hidden="true"
+                                className="absolute inset-x-0 bottom-0 h-1 transition-colors duration-200 ease-out group-data-open:bg-[#b3438f] sm:mt-5 sm:translate-y-px sm:transform"
+                              />
+                            </PopoverButton>
+                          </div>
 
-                      <PopoverPanel
-                        transition
-                        className="absolute inset-x-0 top-full transition data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
-                      >
-                        {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                        <div
-                          aria-hidden="true"
-                          className="absolute inset-0 top-1/2 bg-white shadow-sm"
-                        />
+                          <PopoverPanel
+                            transition
+                            className="absolute inset-x-0 top-full transition data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+                          >
+                            <div
+                              aria-hidden="true"
+                              className="absolute inset-0 top-1/2 bg-white shadow-sm"
+                            />
 
-                        <div className="relative bg-white">
-                          <div className="mx-auto relative z-2 bg-white max-w-7xl px-8">
-                            <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
-                              <div className="grid grid-cols-2 grid-rows-1 gap-8 text-sm">
-                                {category.featured.map((item, itemIdx) => (
-                                  <div
-                                    key={item.name}
-                                    className={classNames(
-                                      itemIdx === 0 ? "col-span-2" : "",
-                                      "group relative overflow-hidden rounded-md bg-gray-100"
-                                    )}
-                                  >
-                                    <img
-                                      alt={item.imageAlt}
-                                      src={item.imageSrc}
-                                      className={classNames(
-                                        itemIdx === 0
-                                          ? "aspect-2/1"
-                                          : "aspect-square",
-                                        "w-full object-cover group-hover:opacity-75"
-                                      )}
-                                    />
-                                    <div className="absolute inset-0 flex flex-col justify-end">
-                                      <div className="bg-white/60 p-4 text-sm">
-                                        <a
-                                          href={item.href}
-                                          className="font-medium text-gray-900"
-                                        >
-                                          <span
-                                            aria-hidden="true"
-                                            className="absolute inset-0"
-                                          />
-                                          {item.name}
-                                        </a>
-                                        <p
-                                          aria-hidden="true"
-                                          className="mt-0.5 text-gray-700 sm:mt-1"
-                                        >
-                                          Пазарувайте сега
-                                        </p>
-                                      </div>
-                                    </div>
-                                  </div>
-                                ))}
-                              </div>
-                              <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-x-8 gap-y-10 text-sm text-gray-500 max-w-full">
-                                {sortedCategories.map((category) => {
-                                  const chunkSize = 10;
-                                  const chunks = [];
-                                  for (
-                                    let i = 0;
-                                    i < category.subcategories.length;
-                                    i += chunkSize
-                                  ) {
-                                    chunks.push(
-                                      category.subcategories.slice(
-                                        i,
-                                        i + chunkSize
-                                      )
-                                    );
-                                  }
-
-                                  return (
-                                    <Fragment key={category.id}>
-                                      {/* Първата колона редом до Artdeco */}
-                                      <div className="space-y-4 min-w-[150px]">
-                                        <h3 className="font-medium text-gray-900 mb-2">
-                                          {category.name}
-                                        </h3>
-                                        <ul className="space-y-2 text-gray-500 text-sm">
-                                          {chunks[0]?.map((subcat) => (
-                                            <li key={subcat.id}>
-                                              <Link
-                                                href={`/category/${subcat.slug}`}
-                                                className="hover:text-gray-800"
-                                              >
-                                                {subcat.name}
-                                              </Link>
-                                            </li>
-                                          ))}
-                                        </ul>
-                                      </div>
-
-                                      {/* Всички останали падат отдолу */}
-                                      {chunks.slice(1).map((chunk, index) => (
-                                        <div
-                                          key={index}
-                                          className="space-y-4 min-w-[150px]"
-                                        >
-                                          <ul className="space-y-2 text-gray-500 text-sm">
-                                            {chunk.map((subcat) => (
-                                              <li key={subcat.id}>
-                                                <Link
-                                                  href={`/category/${subcat.slug}`}
-                                                  className="hover:text-gray-800"
-                                                >
-                                                  {subcat.name}
-                                                </Link>
-                                              </li>
-                                            ))}
-                                          </ul>
+                            <div className="relative bg-white">
+                              <div className="mx-auto relative z-2 bg-white max-w-7xl px-8">
+                                <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
+                                  <div className="grid grid-cols-2 grid-rows-1 gap-8 text-sm">
+                                    {category.featured.map((item, itemIdx) => (
+                                      <div
+                                        key={item.name}
+                                        className={classNames(
+                                          itemIdx === 0 ? "col-span-2" : "",
+                                          "group relative overflow-hidden rounded-md bg-gray-100"
+                                        )}
+                                      >
+                                        <img
+                                          alt={item.imageAlt}
+                                          src={item.imageSrc}
+                                          className={classNames(
+                                            itemIdx === 0
+                                              ? "aspect-2/1"
+                                              : "aspect-square",
+                                            "w-full object-cover group-hover:opacity-75"
+                                          )}
+                                        />
+                                        <div className="absolute inset-0 flex flex-col justify-end">
+                                          <div className="bg-white/60 p-4 text-sm">
+                                            <Link
+                                              href={item.href}
+                                              className="font-medium text-gray-900"
+                                              onClick={close}
+                                            >
+                                              <span
+                                                aria-hidden="true"
+                                                className="absolute inset-0"
+                                              />
+                                              {item.name}
+                                            </Link>
+                                            <p
+                                              aria-hidden="true"
+                                              className="mt-0.5 text-gray-700 sm:mt-1"
+                                            >
+                                              Пазарувайте сега
+                                            </p>
+                                          </div>
                                         </div>
-                                      ))}
-                                    </Fragment>
-                                  );
-                                })}
+                                      </div>
+                                    ))}
+                                  </div>
+                                  <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-x-8 gap-y-10 text-sm text-gray-500 max-w-full">
+                                    {sortedCategories.map((category) => {
+                                      const chunkSize = 10;
+                                      const chunks = [];
+                                      for (
+                                        let i = 0;
+                                        i < category.subcategories.length;
+                                        i += chunkSize
+                                      ) {
+                                        chunks.push(
+                                          category.subcategories.slice(
+                                            i,
+                                            i + chunkSize
+                                          )
+                                        );
+                                      }
+
+                                      return (
+                                        <Fragment key={category.id}>
+                                          <div className="space-y-4 min-w-[150px]">
+                                            <h3 className="font-medium text-gray-900 mb-2">
+                                              {category.name}
+                                            </h3>
+                                            <ul className="space-y-2 text-gray-500 text-sm">
+                                              {chunks[0]?.map((subcat) => (
+                                                <li key={subcat.id}>
+                                                  <Link
+                                                    href={`/category/${subcat.slug}`}
+                                                    className="hover:text-gray-800"
+                                                    onClick={close}
+                                                  >
+                                                    {subcat.name}
+                                                  </Link>
+                                                </li>
+                                              ))}
+                                            </ul>
+                                          </div>
+
+                                          {chunks
+                                            .slice(1)
+                                            .map((chunk, index) => (
+                                              <div
+                                                key={index}
+                                                className="space-y-4 min-w-[150px]"
+                                              >
+                                                <ul className="space-y-2 text-gray-500 text-sm">
+                                                  {chunk.map((subcat) => (
+                                                    <li key={subcat.id}>
+                                                      <Link
+                                                        href={`/category/${subcat.slug}`}
+                                                        className="hover:text-gray-800"
+                                                        onClick={close}
+                                                      >
+                                                        {subcat.name}
+                                                      </Link>
+                                                    </li>
+                                                  ))}
+                                                </ul>
+                                              </div>
+                                            ))}
+                                        </Fragment>
+                                      );
+                                    })}
+                                  </div>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </div>
-                      </PopoverPanel>
+                          </PopoverPanel>
+                        </>
+                      )}
                     </Popover>
                   ))}
 
